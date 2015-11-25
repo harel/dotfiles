abe8660870ea394d0421ff982b160c413d7cbd70
Harel Malka <harel@harelmalka.com>
2015-11-25 01:56:47 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 sudo apt-add-repository ppa:webupd8team/java
+
+sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+
+sudo sh -c 'echo "deb http://www.rabbitmq.com/debian/ testing main" > /etc/apt/sources.list.d/rabbitmq.list'
+wget --quiet -O - https://www.rabbitmq.com/rabbitmq-signing-key-public.asc | sudo apt-key add -
+
 sudo apt-get update
+
 sudo apt-get install screen vim git mercurial htop terminator \
-nodejs npm oracle-java8-installer
+nodejs npm oracle-java8-installer postgresql-9.4 rabbitmq-server \
+
