d995757423ea7ee13f6d24a5cb867a5e8d69119d
Harel Malka <harel@harelmalka.com>
2015-11-25 01:46:05 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-sudo apt-get install screen vim git mercurial htop terminator
+sudo apt-add-repository ppa:webupd8team/java
+sudo apt-get update
+sudo apt-get install screen vim git mercurial htop terminator \
+nodejs npm oracle-java8-installer
