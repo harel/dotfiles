707d3873f5685628c5d3c1b167ac7ac245419796
Harel Malka <harel@harelmalka.com>
2016-01-19 17:50:53 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo apt-add-repository ppa:webupd8team/java
 sudo apt-add-repository ppa:tualatrix/ppa
-sudo apt-add-repository ppa:ansible/ansible
+# sudo apt-add-repository ppa:ansible/ansible
 
 sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
@@ -15,8 +15,8 @@ sudo apt-get install gnome-shell screen vim git mercurial htop terminator \
 nodejs npm oracle-java8-installer postgresql-9.4 postgresql-server-dev-9.4 rabbitmq-server \
 ubuntu-tweak gnome-tweak-tool python-pip mysql-server python-dev  libmysqlclient-dev \
 libssl-dev libcurl4-openssl-dev build-essential libpq-dev libav-dbg \
-software-properties-common ansible
+software-properties-common
 
-sudo pip install virtualenv
+sudo pip install virtualenv ansible
 
 mkdir ~/dev
