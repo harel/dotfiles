73b918868b63398e377c328bfdeb34a3d9790ab4
Harel Malka <harel@harelmalka.com>
2018-12-15 15:08:30 +0000

    sublime dev repo

--- system-setup.sh
+++ system-setup.sh
@@ -19,6 +19,7 @@ wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-
 sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -sc)-pgdg main" > /etc/apt/sources.list.d/PostgreSQL.list'
 # sublime
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+echo "deb https://download.sublimetext.com/ apt/dev/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 apt-get update
 apt-get install postgresql-11 virtualenv virtualenvwrapper git libgtk2.0-0 sublime-text
 
