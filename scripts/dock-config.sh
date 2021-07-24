54ba38bcb62163b9d95d11c254e77665c5afa2f5
Harel Malka <harel@harelmalka.com>
2021-07-24 20:47:30 +0100

    Create dock-config.sh

---
+++ scripts/dock-config.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# source: https://linuxconfig.org/how-to-customize-dock-panel-on-ubuntu-18-04-bionic-beaver-linux
+gsettings set org.gnome.shell.extensions.dash-to-dock extend-height false
+gsettings set org.gnome.shell.extensions.dash-to-dock dock-position BOTTOM
+gsettings set org.gnome.shell.extensions.dash-to-dock transparency-mode FIXED
+gsettings set org.gnome.shell.extensions.dash-to-dock dash-max-icon-size 64
+gsettings set org.gnome.shell.extensions.dash-to-dock unity-backlit-items true
